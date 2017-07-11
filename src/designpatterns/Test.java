7974f1bbd4b14ac31ff35ccdaf231031b67bc78d
jungle <Junglelaw@foxmail.com>
2017-07-11 18:46:07 +0800

    1# commit

--- src/designpatterns/Test.java
+++
@@ -1,7 +0,0 @@
-package designpatterns;
-
-/**
- * Created by Jungle on 2017/7/11.
- */
-public class Test {
-}
---
+++ src/designpatterns/singleton/Singleton.java
@@ -0,0 +1,8 @@
+package designpatterns.singleton;
+
+/**
+ * Created by Jungle on 2017/7/11.
+ */
+public class Singleton {
+
+}
