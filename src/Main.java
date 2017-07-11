769385e1c6122fcff8ac5b022f323ede1c66c46a
jungle <Junglelaw@foxmail.com>
2017-07-11 17:15:37 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jungle on 2017/7/11.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.print("Hello world!");
+    }
+}
---
+++ src/designpatterns/Test.java
@@ -0,0 +1,7 @@
+package designpatterns;
+
+/**
+ * Created by Jungle on 2017/7/11.
+ */
+public class Test {
+}
